6400a62188582fb4f9e07a33dc28ab393fdcd56a
Ankur Fartyal <37195694+ankurf78@users.noreply.github.com>
2018-05-15 18:52:40 +0200

    1c and d

--- Task1c.m
+++ Task1c.m
@@ -1,28 +1,53 @@
 clc
+clear all
+close all
 
 %% Initialization
 
 s = tf('s');
-cp = 0.4;                   %damping coefficient
+dampz=1;
+switch dampz
+    case 1  %for underdamped
+        cp=0.4;
+    case 2  %for critically damped
+        cp=2.0112
+    case 3  %for overdamped
+        cp=3
+end
+% cp =0.4;                   %damping coefficient
 kp = 6.32;                  %spring constant
 mp = 0.16;                  %mass
 cc = 2*sqrt(mp*kp);         %critical damping coefficient
 zeta = cp/cc;               %damping ratio
 omega = sqrt(kp/mp);        %natural frequency
 omega_d = (1-zeta^2)*omega; %damped natural frequency
+% Transfer function
+G1 = (omega^2+(2*zeta*omega*s))/(s^2+omega^2+(2*zeta*omega*s));
+G2 = tf([cp kp],[mp cp kp])
+
 fr=0:0.001:10e2;
-[mag,phase,wout]=bode(T,fr);
+[mag,phase,wout]=bode(G1,fr);
 omega_max=wout(find(mag==max(mag))); %find resonant frequency
 
-% Transfer function
-T = (omega^2+(2*zeta*omega*s))/(s^2+omega^2+(2*zeta*omega*s));
+
+%% Task 1d
+f=0.5;
+dt=0.1;
+Tfinal=10;
+t = 0:dt:Tfinal
+u=0.05*sin(2*pi*f*t);
+figure (3)
+lsim(G2,u,t);        %sine input
+
+figure (4)
+impulse(0.05*G2,1);  %Impulse input
 
 %% Plots
-
-figure(1);
-bode(T,fr)
-hold on
-figure(2);
-step(T)
-xlim([0 5])
-hold on
+% 
+% figure(1);
+% bode(G,fr)
+% hold on
+% figure(2);
+% step(G)
+% xlim([0 5])
+% hold on
