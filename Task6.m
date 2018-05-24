be65bf6c638b1d581949648838dff6c246c45f53
asgardus <gandhiansh95@gmail.com>
2018-05-24 15:39:48 +0200

    Task 6 final

--- Task6.m
+++ Task6.m
@@ -2,7 +2,10 @@ clear;
 clc
 
 %% Initialization 2DOF
-
+f=2;                      %frequency (rad/s)
+dt=0.02;
+Tfinal=10;
+t = 0:dt:Tfinal;
 s = tf('s');
 cp = 0.8;                   %damping coefficient
 kp = 6.32;                  %spring constant
@@ -24,6 +27,10 @@ for i=1:length(T_mat)
     simout = sim('task6ss');
     track(i) = T;
 end
+
+% State space to TF Derivative controlled
+[b,a]=ss2tf(A,B,C,D,1);
+skyhook = tf(b(1,:),a);
 
 % Transfer functions
 Num = (cp*cs*s^2+(kp*cs+ks*cp)*s+kp*ks);
@@ -51,6 +58,15 @@ fr=0:0.001:10e2;
 [mag,phase,wout]=bode(T1,fr);
 omega_max2=wout(find(mag==max(mag))); %find resonant frequency
 
+%% Sine excitation
+u=0.05*sin(f*t);
+figure(3);
+lsim(T2,u,t);
+hold on
+plot(z_s,'r');
+legend('show')
+
+
 %% Plots
 
 figure(1);
@@ -63,8 +79,11 @@ hold on
 figure(1);
 bode(T2,fr)
 hold on
+bode(skyhook,fr)
 legend('show')
 figure(2);
 step(T2)
+hold on
+step(skyhook)
 legend('show')
 hold on
